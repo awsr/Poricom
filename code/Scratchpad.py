8c8d884faa6e85d6b21194fe900a0b74e95f28b9
awsr <43862868+awsr@users.noreply.github.com>
2023-03-06 20:32:26 -0800

    Add Append action shortcut and context menu

--- code/Scratchpad.py
+++ code/Scratchpad.py
@@ -2,7 +2,8 @@
 Simple little scratchpad
 """
 
-from PyQt5.QtWidgets import (QTextEdit)
+from PyQt5.QtWidgets import (QTextEdit, QShortcut, QApplication)
+from PyQt5.QtGui import (QContextMenuEvent)
 
 
 class Scratchpad(QTextEdit):
@@ -13,3 +14,14 @@ class Scratchpad(QTextEdit):
         self.setAcceptRichText(False)
         self.setObjectName("scratchpad")
         tracker.scratchpad = self
+
+        self.addTextSc = QShortcut("Ctrl+Shift+V", self)
+        self.addTextSc.activated.connect(lambda: self.append(QApplication.clipboard().text()))
+
+    def contextMenuEvent(self, e: QContextMenuEvent) -> None:
+        self.menu = self.createStandardContextMenu()
+        self.appendAction = self.menu.addAction("Append", lambda: self.append(QApplication.clipboard().text()), "Ctrl+Shift+V")
+        self.appendSeparator = self.menu.addSeparator()
+        self.menu.insertAction(self.menu.actions()[0], self.appendSeparator)
+        self.menu.insertAction(self.appendSeparator, self.appendAction)
+        self.menu.exec(e.globalPos())
