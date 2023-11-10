38fbbc39ef6fa3466e8be3f8b9b0843a2aac8561
awsr <43862868+awsr@users.noreply.github.com>
2023-11-09 18:24:38 -0800

    Remove unnecessary enumerate

--- code/TextHandler.py
+++ code/TextHandler.py
@@ -6,11 +6,11 @@ class TextFormatter():
     def __init__(self):
         self._rules = None
 
-    def set_rules(self, rules):
+    def set_rules(self, rules: list):
         self._rules = rules
 
     def process(self, text):
-        for i, rule in enumerate(self._rules):
+        for rule in self._rules:
             text = text.replace(rule[0], rule[1])
 
         return text
